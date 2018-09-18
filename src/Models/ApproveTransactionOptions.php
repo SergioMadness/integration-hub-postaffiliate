c0f0129b2ae57a9b9a28483f9d5180bc6ec51f74
SergioMadness <madness.sergio@gmail.com>
2018-09-18 18:49:15 +0300

    Describe subsystem options

---
+++ src/Models/ApproveTransactionOptions.php
@@ -0,0 +1,20 @@
+<?php namespace professionalweb\IntegrationHub\Postaffiliate\Models;
+
+/**
+ * Subsystem options
+ * @package professionalweb\IntegrationHub\Postaffiliate\Models
+ */
+class ApproveTransactionOptions extends NewEventOptions
+{
+
+
+    /**
+     * Get available fields for mapping
+     *
+     * @return array
+     */
+    public function getAvailableFields(): array
+    {
+        return [];
+    }
+}
